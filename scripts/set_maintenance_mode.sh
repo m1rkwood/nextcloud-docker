c97df65d5a4d078249616e1952e213348ff1538d
m1rkwood <maxime@f4ng0rn-home.local>
2021-11-12 10:51:35 -0500

    adjusting maintenance mode script

--- scripts/set_maintenance_mode.sh
+++ scripts/set_maintenance_mode.sh
@@ -4,7 +4,13 @@
 MAINTENANCE_MODE=$1
 
 if [ ! -z "$MAINTENANCE_MODE" ]; then
-    docker exec -it nextcloud sudo -u www-data php -d memory_limit=-1 /var/www/html/occ maintenance:mode --MAINTENANCE_MODE
+    if [ $MAINTENANCE_MODE = "on" ]; then
+      docker exec -it nextcloud sudo -u www-data php -d memory_limit=-1 /var/www/html/occ maintenance:mode --on
+    fi
+
+    if [ $MAINTENANCE_MODE = "off" ]; then
+      docker exec -it nextcloud sudo -u www-data php -d memory_limit=-1 /var/www/html/occ maintenance:mode --off
+    fi
 else
     echo '[!] Missing maintenance mode (on/off) as argument'
 fi
